4a82077d93db755b7568c4bd80fb88784ca105ad
Josh Feng <joshuafeng2@gmail.com>
2022-08-24 21:25:24 -0400

    expand stat module

--- examples/stat.lua
+++ examples/stat.lua
@@ -5,7 +5,9 @@
 --  - Hoel, Port, Stone,
 --  - Doob, "Stochastic Processes", 1971
 --  - Feller,
+--  <http://www.circuitwizard.de/lmpfrlib/lmpfrlib.html>
 local class = require('pool')
+-- local complex = require('complex') -- for characteristic function (FFT)
 
 local tsort, tconcat, tunpack = table.sort, table.concat, table.unpack
 local mfloor, msqrt = math.floor, math.sqrt
@@ -28,38 +30,92 @@ local sample = class { -- {{{ pseudo random variable
         for i = 2, #(o.dims) do total = total * o.dims[i] end
         if type(f) == 'function' then -- initialize
             for i = 1, total do o[i] = f(o, i) end
-        else -- TODO 0/1/...
-            for i = 1, total do o[i] = 0 end
+        else
+            f = f and 1 or 0
+            for i = 1, total do o[i] = f end
         end
     end; -- }}}
 
-    -- ['>'] = function (o) end;
-
-    { -- operators (cycle rule)
-        __add = function (o1, o2) -- TODO
+    ['^'] = { -- operators (cycle rule)
+        __add = function (o1, o2)
             local n1, n2 = #o1, #o2
-            local n = class:new(o)
-            if n1 >= n2 then
-            else
+            local o = class:new(o1)
+            if n1 == 0 or n2 == 0 then return o end
+            if n1 > n2 then for i = 1, n1 do o[i] = o1[i] + o2[(i - 1) % n2 + 1] end
+            else            for i = 1, n2 do o[i] = o1[(i - 1) % n1 + 1] + o2[i] end end
+            return o
+        end;
+
+        __sub = function (o1, o2)
+            local n1, n2 = #o1, #o2
+            local o = class:new(o1)
+            if n1 == 0 or n2 == 0 then return o end
+            if n1 > n2 then for i = 1, n1 do o[i] = o1[i] - o2[(i - 1) % n2 + 1] end
+            else            for i = 1, n2 do o[i] = o1[(i - 1) % n1 + 1] - o2[i] end end
+            return o
+        end;
+
+        __mul = function (o1, o2)
+            local n1, n2 = #o1, #o2
+            local o = class:new(o1)
+            if n1 == 0 or n2 == 0 then return o end
+            if n1 > n2 then for i = 1, n1 do o[i] = o1[i] * o2[(i - 1) % n2 + 1] end
+            else            for i = 1, n2 do o[i] = o1[(i - 1) % n1 + 1] * o2[i] end end
+            return o
+        end;
+
+        __div = function (o1, o2)
+            local n1, n2 = #o1, #o2
+            local o = class:new(o1)
+            if n1 == 0 or n2 == 0 then return o end
+            if n1 > n2 then for i = 1, n1 do o[i] = o1[i] / o2[(i - 1) % n2 + 1] end
+            else            for i = 1, n2 do o[i] = o1[(i - 1) % n1 + 1] / o2[i] end end
+            return o
+        end;
+
+        __eq = function (o1, o2)
+            if #o1 ~= #o2 then return false end
+            for i = 1, #o1 do if o1[i] ~= o2[i] then return false end end
+            return true
+        end;
+
+        __band = function (o1, o2) -- bitwise AND & : convolution
+            if #o1 == 0 or #o2 == 0 then return end
+            local o = class:new(o1)
+            for i = 1, (#o1 + #o2 - 1) do
+                local v = 0
+                for j = 1, #o2 do -- TODO
+                    if i >= j then v = v + o1[i - j + 1] * o2[j] else break end
+                end
+                o[i] = v
             end
-            return n
+            return o
         end;
 
-        __sub = function (o1, o2) -- TODO
+        __bor = function (o1, o2) -- bitwise OR | : inner product
+            if #o1 ~= #o2 then return end
+            local v = 0
+            for i = 1, #o1 do v = v + o1[i] * o2[i] end
+            return v
         end;
 
-        __mul = function (o1, o2) -- TODO
-        end;
-
-        __div = function (o1, o2) -- TODO
-        end;
+        -- __pow = function (o1, o2) end; -- power ^ : norm?
+        -- __mod = function (o1, o2) end; -- modulo % : regression?
+        -- __idiv = function (o1, o2) end; -- floor division // : norm?
+        -- __unm = function (o1, o2) end; -- negation -
+        -- __bnot   = function (o1, o2) end; -- bitwise not ~ : transpose?
+        -- __bxor   = function (o1, o2) end; -- bitwise exclusive OR ~
+        -- __shl    = function (o1, o2) end; -- bitwise shift left <<
+        -- __shr    = function (o1, o2) end; -- bitwise shift right >>
+        -- __concat = function (o1, o2) end; -- concatination ..
+        -- __len    = function (o1, o2) end; -- transpose #
+        -- __lt     = function (o1, o2) end; -- less than < (not defined)
+        -- __le     = function (o1, o2) end; -- less equal <= (not defined)
+        -- __call = function (o, mode) end; -- ()fast-copy/(0)/(false)
 
         __tostring = function (o) -- TODO dims/average/std.dev
             return o.dims and tconcat(o.dims, ':') or ''
         end;
-
-        -- convolution
-        -- regression / ...
     };
 
     -- member functions / statistics (sample)
@@ -70,6 +126,11 @@ local sample = class { -- {{{ pseudo random variable
     end; -- }}}
 
     map = function (o, f) for i = 1, #o do o[i] = f(o, i) end end;
+
+    fft = function (o1, o2) --{{{ complex characteristic function? TODO
+        local f1, f2 -- real and complex part array
+        return f1, f2
+    end; --}}}
 
     sort = table.sort;
 
@@ -103,9 +164,10 @@ local sample = class { -- {{{ pseudo random variable
             v = v - mean
             cvar = cvar + v * v
         end
-        cvar = cvar / (#o - 1)
-        -- TODO kertosis
         return mean, cvar
+    end; --}}}
+
+    property = function (o, rv) --{{{ TODO skewness, kertosis, ...
     end; --}}}
 } -- }}}
 
@@ -115,7 +177,7 @@ local stat = {
 }
 
 setmetatable(stat, {
-    __metatable = true;
+    __metatable = "MIT license";
     __call = function (c, ...) return sample(...) end;
 })
 
@@ -147,7 +209,15 @@ c = stat(10, 10, 10, function (o, i) return (i - 1) / 1000 end)
 print(c:cmnt())
 print(c:mnt(2))
 print(c:range())
+a = stat(2)
+b = stat(2)
+a[1], a[2] = 1, 1
+b[1], b[2] = 1, 1
+-- c = a & b
+-- print(c[1], c[2], c[3])
 
+
+for k, v in pairs(stat.api) do print(k, v) end
 
 return stat
 -- ==================================================================== --
